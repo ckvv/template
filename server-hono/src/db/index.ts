0ea5d1a3b85fa0321aea47f955f88d2900d671e3
ckvv <k_0520@qq.com>
2025-04-03 17:39:15 +0800

    fix: cloudflare 数据库不支持 tcp 链接

--- server-hono/src/db/index.ts
+++ server-hono/src/db/index.ts
@@ -1,9 +1,25 @@
+import type { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import process from 'node:process';
 import { config } from '#config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http';
+import { drizzle as drizzleNode } from 'drizzle-orm/node-postgres';
 import * as schema from './schema.js';
 
 export * from './schema.js';
 
-export const db = drizzle(config.DATABASE_URL, {
-  schema,
-});
+// eslint-disable-next-line import/no-mutable-exports
+let db: NodePgDatabase<typeof schema> | NeonHttpDatabase<typeof schema>;
+
+if (process?.release?.name === 'node') {
+  db = drizzleNode(config.DATABASE_URL, {
+    schema,
+  });
+}
+else {
+  db = drizzleNeon(config.DATABASE_URL, {
+    schema,
+  });
+}
+
+export { db };
