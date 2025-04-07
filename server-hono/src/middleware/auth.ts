07b4f7c90c2d499ef5bc0f858d200a33d50782d6
ckvv <k_0520@qq.com>
2025-04-07 13:32:02 +0800

    Update auth.ts

--- server-hono/src/middleware/auth.ts
+++ server-hono/src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { CustomError, jwt } from '#utils';
 import { getCookie } from 'hono/cookie';
 import { createMiddleware } from 'hono/factory';
 
-const whiteList = ['/auth/signup', '/'];
+const whiteList = ['/auth/signup', '/auth/signin', '/'];
 
 function isInWhiteList(path: string) {
   return whiteList.includes(path);
