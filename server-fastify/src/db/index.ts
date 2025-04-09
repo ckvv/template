c147ca3d60d715b1d8e3f2e37699e543ab776e4f
ckvv <k_0520@qq.com>
2025-04-09 17:59:28 +0800

    chore: 删除 neon

--- server-fastify/src/db/index.ts
+++ server-fastify/src/db/index.ts
@@ -1,24 +1,9 @@
-import type { NeonHttpDatabase } from 'drizzle-orm/neon-http';
-import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { config } from '#config';
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http';
 import { drizzle as drizzleNode } from 'drizzle-orm/node-postgres';
 import * as schema from './schema.js';
 
 export * from './schema.js';
 
-// eslint-disable-next-line import/no-mutable-exports
-let db: NodePgDatabase<typeof schema> | NeonHttpDatabase<typeof schema>;
-
-if (config.IS_NODE) {
-  db = drizzleNode(config.DATABASE_URL, {
-    schema,
-  });
-}
-else {
-  db = drizzleNeon(config.DATABASE_URL, {
-    schema,
-  });
-}
-
-export { db };
+export const db = drizzleNode(config.DATABASE_URL, {
+  schema,
+});
