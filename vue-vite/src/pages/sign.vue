ffe6e50fa0a55edb332d769bf874c7c2cfa85824
ckvv <k_0520@qq.com>
2025-04-14 19:36:08 +0800

    添加注册功能

--- vue-vite/src/pages/sign.vue
+++ vue-vite/src/pages/sign.vue
@@ -3,14 +3,17 @@ import { authAPI } from '@/api';
 import { signSchema } from '@/constant/schema';
 import { router, useRoute } from '@/router';
 import { useSignStore } from '@/stores/sign';
-import { ref } from 'vue';
+import { ref, useTemplateRef } from 'vue';
+
+type TYPE = 'signin' | 'signup';
 
 definePage({
   name: 'sign',
 });
 
+const formRef = useTemplateRef('formRef');
 const toast = useToast();
-const type = ref<'signin' | 'signup'>('signin');
+const type = ref<TYPE>('signin');
 const signStore = useSignStore();
 const route = useRoute();
 
@@ -19,8 +22,7 @@ const user = ref({
   password: '',
 });
 
-const { execute: signin, isFetching: isSigning, onFetchResponse: onSignin } = authAPI.signin(user);
-
+const { execute: signin, isFetching: isSignin, onFetchResponse: onSignin } = authAPI.signin(user);
 onSignin(({ data }) => {
   if (data?.code !== 0)
     return;
@@ -29,34 +31,57 @@ onSignin(({ data }) => {
   router.push({ path: route.query?.to as string ?? '/auth' });
 });
 
+const { execute: signup, isFetching: isSignUp, onFetchResponse: onSignUp } = authAPI.signup(user);
+onSignUp(({ data }) => {
+  if (data?.code !== 0)
+    return;
+  toast.add({ description: '注册成功', color: 'success' });
+  handlerSwitch('signin');
+});
+
+function onSubmit() {
+  switch (type.value) {
+    case 'signin':
+      signin();
+      break;
+    case 'signup':
+      signup();
+      break;
+    default:
+      break;
+  }
+}
+
 function signinGithub() {
   window.open('https://github.com/ckvv');
 }
 
-function _signup() {
-  type.value = 'signin';
+function handlerSwitch(value?: TYPE) {
+  formRef.value?.setErrors([]);
+
+  type.value = value || type.value === 'signup' ? 'signin' : 'signup';
 }
 </script>
 
 <template>
   <div class="text-center select-none">
-    <UForm :schema="signSchema" :state="user" class="w-2xl flex flex-col gap-2 m-auto" @submit="signin()">
-      <UFormField label="Username" name="username" class="text-left">
+    <UForm ref="formRef" :schema="signSchema" :state="user" class="w-2xl max-w-full flex flex-col gap-2 m-auto" @submit="onSubmit()">
+      <UFormField label="用户名" name="username" class="text-left">
         <UInput v-model="user.username" class="w-full" />
       </UFormField>
-      <UFormField label="Password" name="password" class="text-left">
+      <UFormField label="密码" name="password" class="text-left">
         <UInput v-model="user.password" class="w-full" />
       </UFormField>
-      <div v-if="type === 'signin'" class="flex flex-col gap-2">
-        <UButton class="w-full justify-center" type="submit" :loading="isSigning">
-          登录
+      <div class="flex flex-col gap-2">
+        <UButton class="w-full justify-center" type="submit" :loading="isSignin || isSignUp">
+          {{ type === 'signin' ? '登录' : '注册' }}
         </UButton>
         <div class="flex justify-between items-center">
           <span class="flex">
             第三方登录: <UIcon name="i-bytesize-github" class="size-6 ml-2 cursor-pointer" @click="signinGithub" />
           </span>
-          <UButton link variant="link" @click="type = 'signup'">
-            注册
+          <UButton link variant="link" @click="handlerSwitch()">
+            {{ type !== 'signin' ? '登录' : '注册' }}
           </UButton>
         </div>
       </div>
